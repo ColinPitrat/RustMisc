9da9ac4765a7d1ace6b4e625a6bdb25bad946324
Colin Pitrat <colin.pitrat@gmail.com>
2019-07-29 23:48:42 +0100

    Make ReLu more parametrizable and provide a name() method on activation function.

--- neuralnet_mnist/src/activation.rs
+++ neuralnet_mnist/src/activation.rs
@@ -3,7 +3,9 @@
 #[cfg(test)] use assert_approx_eq::assert_approx_eq;
 use serde::{Serialize,Deserialize};
 
-pub const RELU: ReLu = ReLu{};
+pub const RELU: ReLu = ReLu{alpha: 0.0, beta: 0.0, gamma: 1.0, t1: 0.0, t2: 0.0};
+pub const RELU6: ReLu = ReLu{alpha: 0.0, beta: 1.0, gamma: 0.0, t1: 0.0, t2: 6.0};
+pub const LEAKYRELU: ReLu = ReLu{alpha: 0.01, beta: 0.0, gamma: 1.0, t1: 0.0, t2: 0.0};
 pub const SIGMOID: Sigmoid = Sigmoid{};
 pub const TANH: TanH = TanH{};
 
@@ -11,27 +13,46 @@ pub const TANH: TanH = TanH{};
 pub trait ActivationFunction {
     fn value(&self, x: f64) -> f64;
     fn derivative(&self, x: f64) -> f64;
+    fn name(&self) -> String;
 }
 
 #[derive(Serialize,Deserialize)]
-pub struct ReLu;
+pub struct ReLu {
+    // Slope before t1
+    pub alpha: f64,
+    // Slope between t1 and t2
+    pub beta: f64,
+    // Slope after t2
+    pub gamma: f64,
+
+    pub t1: f64,
+    pub t2: f64,
+}
 
 #[typetag::serde]
 impl ActivationFunction for ReLu {
     fn value(&self, x: f64) -> f64 {
-        if x > 0.0 {
-            x
+        if x > self.t2 {
+            self.gamma*x
+        } else if x > self.t1 {
+            self.beta*x
         } else {
-            0.0
+            self.alpha*x
         }
     }
 
     fn derivative(&self, x: f64) -> f64 {
-        if x > 0.0 {
-            1.0
+        if x > self.t2 {
+            self.gamma
+        } else if x > self.t1 {
+            self.beta
         } else {
-            0.0
+            self.alpha
         }
+    }
+
+    fn name(&self) -> String {
+        format!("ReLu({} <{}, {} <{}, {})", self.alpha, self.t1, self.beta, self.t2, self.gamma)
     }
 }
 
@@ -53,6 +74,10 @@ impl ActivationFunction for Sigmoid {
             e_x/(1.0+2.0*e_x+e_x*e_x)
         }
     }
+
+    fn name(&self) -> String {
+        String::from("Sigmoid")
+    }
 }
 
 #[derive(Serialize,Deserialize)]
@@ -67,6 +92,10 @@ impl ActivationFunction for TanH {
     fn derivative(&self, x: f64) -> f64 {
         let th = x.tanh();
         1.0-th*th
+    }
+
+    fn name(&self) -> String {
+        String::from("TanH")
     }
 }
 
