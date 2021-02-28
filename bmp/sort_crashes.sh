d9fdd2171f6e813f63907748c83e94d5078c225d
Colin Pitrat <colin.pitrat@gmail.com>
2021-02-28 17:16:29 +0000

    Fix the sort_crashes.sh script

--- bmp/sort_crashes.sh
+++ bmp/sort_crashes.sh
@@ -2,13 +2,14 @@
 
 echo -n "" > crashing.txt
 echo -n "" > not_crashing.txt
-for file in `find tests/ -name \*.bmp`
+find tests/ -name \*.bmp | while read file
 do
   echo "$file"
   cargo run --quiet parse $file > /dev/null
   if [ $? -eq 0 ]
   then
     echo $file >> not_crashing.txt
-  else echo $file >> crashing.txt
+  else
+    echo $file >> crashing.txt
   fi
 done
