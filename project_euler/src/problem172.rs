45fca85d83b5933f51de4d0b8edd7a93f4ec1bf8
Colin Pitrat <colin.pitrat@gmail.com>
2025-01-23 21:11:37 +0000

    Faster version, though less clear

--- project_euler/src/problem172.rs
+++ project_euler/src/problem172.rs
@@ -1,7 +1,12 @@
+use crate::log_verbose;
 use std::collections::HashMap;
 
 struct Calculator {
     memoize: HashMap<(usize, Vec<usize>), usize>,
+}
+
+fn permutations(n: usize, p: usize) -> usize {
+    (n-1)*((n-p+1)..n).product::<usize>()
 }
 
 impl Calculator {
@@ -11,12 +16,18 @@ impl Calculator {
         }
     }
 
-    /// Returns the number of numbers with `digits` digits with at most `repeats`[k]
-    /// occurrences of the digit k.
-    /// This includes numbers starting with leading 0
-    fn n_r(&mut self, digits: usize, repeats: &mut [usize]) -> usize {
+    fn n_r(&mut self, digits: usize, repeats: &mut [usize], max_repeats: usize, so_far: usize) -> usize {
         if digits == 0 {
-            return 1;
+            // Number of distinct digits.
+            let p = repeats.iter()
+                .filter(|&r| *r != max_repeats)
+                .count();
+            // Number of ways to permutate the selected digits, not allowing the first one to be a
+            // 0 (so it has repeats.len()-1 possibilities).
+            let n = repeats.len();
+            let perms = permutations(n, p);
+            log_verbose!("Signature: {repeats:?} - {so_far} - {perms}");
+            return permutations(n, p);
         }
         let repeats_vec = repeats.to_vec();
         if self.memoize.contains_key(&(digits, repeats_vec.clone())) {
@@ -28,8 +39,11 @@ impl Calculator {
                 continue;
             }
             repeats[i] -= 1;
-            result += self.n_r(digits-1, repeats);
+            result += self.n_r(digits-1, repeats, max_repeats, so_far*10+(9-i));
             repeats[i] += 1;
+            if repeats[i] == max_repeats {
+                break;
+            }
         }
         self.memoize.insert((digits, repeats_vec.clone()), result);
         result
@@ -38,35 +52,29 @@ impl Calculator {
     /// Returns the number of numbers with `digits` digits with at most `repeats`[k]
     /// occurrences of the digit k.
     /// This excludes numbers starting with leading 0
-    fn n_r_no0(&mut self, digits: usize, repeats: &mut [usize]) -> usize {
+    // The overall idea is to produce all the possible combinations of digit to a permutation.
+    // So 9987, 1123, 5572, ... are all equivalent forms aabc for a choice of aabc where each
+    // letter is a different digit and a != 0.
+    // Any number will match a single of these combinations.
+    // The number of numbers matching a given combination is the number of permutations possibles.
+    // With leading 0s, this would simply be 10*9*...*(10-k) where k is the number of distinct
+    // digits. As we forbid leading 0s, this is instead 9*9*...*(10-k).
+    fn n_r_no0(&mut self, digits: usize, repeats: &mut [usize], max_repeats: usize, so_far: usize) -> usize {
         if digits == 0 {
             return 1;
         }
         let mut result = 0;
-        for i in 1..=9 {
-            if repeats[i] == 0 {
-                continue;
-            }
-            repeats[i] -= 1;
-            result += self.n_r(digits-1, repeats);
-            repeats[i] += 1;
-        }
+        repeats[0] -= 1;
+        result += self.n_r(digits-1, repeats, max_repeats, so_far*10+9);
+        repeats[0] += 1;
         result
     }
 
     /// Returns the number of numbers with `digits` digits with at most `repeats`
     /// occurrences of any digit.
     /// This includes numbers starting with leading 0
-    #[allow(dead_code)]
-    fn n_(&mut self, digits: usize, repeats: usize) -> usize {
-        self.n_r(digits, vec!(repeats; 10).as_mut_slice())
-    }
-
-    /// Returns the number of numbers with `digits` digits with at most `repeats`
-    /// occurrences of any digit.
-    /// This includes numbers starting with leading 0
     fn n_no0(&mut self, digits: usize, repeats: usize) -> usize {
-        self.n_r_no0(digits, vec!(repeats; 10).as_mut_slice())
+        self.n_r_no0(digits, vec!(repeats; 10).as_mut_slice(), repeats, 0)
     }
 }
 
@@ -74,11 +82,6 @@ pub fn solve(digits: usize, repeats: usize) -> usize {
     #[cfg(debug_assertions)]
     println!("Note: build with --release for faster execution.");
 
-    // This is just a brute force approach with memoization that builds all the possibilities.
-    // We could do something much smarter.
-    // One first idea of improvement is to force numbers to appear (for their first occurrence) in
-    // a specific order (e.g. 1-9 then 0) and then multiply the result by 9.9! as we can do all
-    // permutations we want except those that result to a leading 0.
     let mut calc = Calculator::new();
     calc.n_no0(digits, repeats)
 }
@@ -88,16 +91,8 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_n_() {
-        let mut calc = Calculator::new();
-        for n in 0..5 {
-            assert_eq!(10_usize.pow(n as u32), calc.n_(n, n));
-        }
-        assert_eq!(9990, calc.n_(4, 3));
-    }
-
-    #[test]
     fn test_solve() {
         assert_eq!(8991, solve(4, 3));
+        assert_eq!(227485267000992000, solve(18, 3));
     }
 }
